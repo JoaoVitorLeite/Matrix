d8cc4d977fc3190e9d65c85686db12e3cbf9114e
Joao <JoaoVitorLeite>
2020-05-31 16:33:41 -0300

    31/05 - 16:33

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Matrix
+
+### Description
+
 Class implementation to represent matrices in Scala
+
+### About
+
+This class was implemented using a generic type, which was implemented [here](https://github.com/JoaoVitorLeite/Matrix/blob/master/src/main/scala/matrix/Num.scala), which supports some basic algebraic operations and is defined for types Int, Float, Double, Long. The tests made are of simple character, and for this the Scala Test was used. In total 27 tests were performed.
+
+### Project Structure
+
+```bash
+src
+|
++---.gitignore
+|   build.sbt
+|   LICENSE
+|   README.md
+|                               
++---src
+|   +---main
+|   |   \---scala
+|   |       \---matrix
+|   |               Matrix.scala
+|   |               Num.scala
+|   |               
+|   \---test
+|       \---scala
+|           \---matrix
+|                   MatrixSuite.scala
+|                   
+
+```
+
+### Compile
+
+To compile the files it is necessary to be in the root directory(where 
+the `build.sbt` file is), the use the `sbt compile` command.
+
+### Test
+
+To perform the tests it is necessary to be in the root 
+directory(where the `build.sbt` file is), then use the `sbt test` 
+command. The tests used FunSuite and assert's.
+
+### IDE
+
+The IDE used was [Intellij Idea](https://www.jetbrains.com/idea/).
+
+### References
+
+* [Link](https://www.mathsisfun.com/algebra/matrix-introduction.html)
+* [Link](https://www.mathwords.com/i/inverse_of_a_matrix.htm)
+
+### License
+
+The LICENSE used is [Apache-2.0](https://github.com/JoaoVitorLeite/Matrix/blob/master/LICENSE).
--- src/main/scala/matrix/Matrix.scala
+++ src/main/scala/matrix/Matrix.scala
@@ -1,42 +1,111 @@
 package matrix
 
-
 import Num.Implicits._
 
+/***
+ * @author João Vitor Leite
+ */
+
+/***
+ * Class to represent Matrix in Scala
+ * @param row Number of rows
+ * @param col Number of columns
+ * @param values List of Lits with the values
+ * @param op implicit for type class
+ * @tparam A Type class
+ */
 class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[A]){
 
+  /***
+   * Method apply to get a row from the matrix
+   * @param row Index of the row
+   * @return List of values
+   */
   def apply(row: Int): scala.List[A] = values(row)
 
+  /***
+   * Method to get a value from the matrix
+   * @param row Index of the row
+   * @param col Index of the column
+   * @return A value from the matrix
+   */
   def apply(row: A, col: A): A = values(row.toINT)(col.toINT)
 
+  /***
+   * Private method for adding or subtracting Lists
+   * @param xs A list
+   * @param ys A list
+   * @param f Function to apply in the lists
+   * @return List of values
+   */
   private def ADSUBVector(xs: List[A], ys: List[A])(f: (A,A) => A): List[A] = (xs,ys) match{
     case (_, Nil) => xs
     case (Nil, _) => ys
     case (x::xs1, y::ys1) => f(x,y)::ADSUBVector(xs1,ys1)(f)
   }
 
+  /***
+   * Method to perform the addition of matrices
+   * @param other A matrix
+   * @return Matrix resulting from the addition of two matrices
+   */
   def + (other: Matrix[A]): Matrix[A] = {
     require(other.row == row && other.col == col)
     new Matrix[A](row, col, (for(i <- 0 until row.toINT)yield ADSUBVector(this(i), other(i))((x,y) => x+y)).toList)
   }
 
+  /***
+   * Method to perform the subtraction of matrices
+   * @param other A matrix
+   * @return Matrix resulting from the subtraction of two matrices
+   */
   def - (other: Matrix[A]): Matrix[A] = {
     require(other.row == row && other.col == col)
     new Matrix[A](row, col, (for(i <- 0 until row.toINT)yield  ADSUBVector(this(i), other(i))((x,y) => x-y)).toList)
   }
 
+  /***
+   * Method to perform the multiplication by a constant
+   * @param const A constant
+   * @return Matrix resulting from the multiplication by a constant
+   */
   def * (const: A): Matrix[A] = {
     new Matrix[A](row, col, values.map(x => x.map(_*const)))
   }
 
+  /***
+   * Method for finding the "negative matrix
+   * @return Matrix with all members multiplied by -1
+   */
   def unary_- : Matrix[A] = new Matrix[A](row, col, values.map(x => x.map(-_)))
 
+  /***
+   * Method to get a row from the matrix
+   * @param r Index of the row
+   * @return List of values
+   */
   def Row(r: Int): List[A] = values(r)
 
+  /***
+   * Method to get a column from the matrix
+   * @param c Index of the column
+   * @return List of values
+   */
   def Col(c: Int): List[A] = values.map(x => x(c))
 
+  /***
+   * Method for calculating the inner product between two lists of values
+   * @param v1 A list
+   * @param v2 A list
+   * @return A number
+   */
   private def multipleSum(v1: List[A], v2: List[A]): A = (for(i <- v1.indices) yield v1(i)*v2(i)).toList.foldLeft(op.zero)((x,y) => x+y)
 
+  /***
+   * Mdthod to perform de multiplication of matrices
+   * @param other A matrix
+   * @return Matrix resulting from the multiplication of two matrices
+   */
   def * (other: Matrix[A]): Matrix[A] = {
     require(col == other.row)
     val res = (for{
@@ -46,8 +115,18 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix[A](row, other.col, res)
   }
 
+  /***
+   * Method to find the transpose matrix
+   * @return Transpose matrix
+   */
   def T: Matrix[A] = new Matrix[A](col, row, (for(i <- 0 until col.toINT) yield Col(i)).toList)
 
+  /***
+   * Method to remove element on a given index from a list
+   * @param xs A list
+   * @param pos Index to remove the element from the list
+   * @return List without the element in the passed position
+   */
   private def filterElem(xs: List[A], pos: Int): List[A] = {
     @scala.annotation.tailrec
     def loop(n: Int, acc: List[A]): List[A] = {
@@ -58,6 +137,12 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     loop(0, Nil)
   }
 
+  /***
+   * Cofactor of element from the matrix
+   * @param r Row of the cofactor
+   * @param c Column of the cofactor
+   * @return Cofactor matrix of Crc
+   */
   def cofactor(r: Int, c: Int): Matrix[A] = {
     val aux = (for{
       i <- 0 until row.toINT
@@ -66,6 +151,10 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix[A](row-op.one, col - op.one, aux.map(x => filterElem(x, c)))
   }
 
+  /***
+   * Determinant
+   * @return Determinant of the matrix
+   */
   def determinant: Long = {
     if(row == 1 && col == 1) values.head.head.toLONG
     else if (row == 2 && col == 2) (values.head.head * values(1)(1) - values.head(1) * values(1).head).toLONG
@@ -76,12 +165,22 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     }
   }
 
+  /***
+   * Identity matrix
+   * @param row Number of rows
+   * @param col Number of columns
+   * @return Identity matrix
+   */
   def identityMatrix(row: A, col: A): Matrix[A] = {
     require(row == col)
     val aux = List.fill(row.toINT, col.toINT)(op.zero)
     new Matrix[A](row, col, (for(i <- 0 until row.toINT)yield aux(i).updated(i, op.one)).toList)
   }
 
+  /***
+   * Cofactor matrix
+   * @return Cofactor matrix
+   */
   def cofactorMatrix: Matrix[Long] = {
     val res = (for{
       i <- 0 until row.toINT
@@ -90,8 +189,16 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix(row.toLONG, col.toLONG, res)
   }
 
+  /***
+   * Adjoint matrix
+   * @return Adjoint matrix
+   */
   def adjointMatrix: Matrix[Long] = cofactorMatrix.T
 
+  /***
+   * Inverse matrix
+   * @return Inverse Matrix
+   */
   def inverse: Matrix[Double] = {
     val det = determinant
     require(det != 0)
@@ -102,8 +209,17 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix[Double](row.toDOUBLE, col.toDOUBLE, res.toList)
   }
 
+  /***
+   * Values from the matrix
+   * @return List of list with the values from the matrix
+   */
   def Values: List[List[A]] = values
 
+  /***
+   * Method to join matrices by rows
+   * @param other A matrix
+   * @return A matrix
+   */
   def joinByRow(other: Matrix[A]): Matrix[A] ={
     @scala.annotation.tailrec
     def foo(ys: List[List[A]], acc: List[List[A]]): List[List[A]] ={
@@ -113,6 +229,11 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix[A](row+other.row, col, foo(other.Values, this.values))
   }
 
+  /***
+   * Method to join matrices by columns
+   * @param other A matrix
+   * @return A matrix
+   */
   def joinByCol(other: Matrix[A]): Matrix[A] ={
     val res = for{
       i <- 0 until other.row.toINT
@@ -120,12 +241,21 @@ class Matrix[A](val row: A, val col: A, values: List[List[A]])(implicit op: Num[
     new Matrix[A](row, col + other.col, res.toList)
   }
 
+  /***
+   * Method to compare matrices
+   * @param obj Any object
+   * @return Return true if the matrices are equal, otherwise false
+   */
   override def equals(obj: Any): Boolean = obj match {
     case v: Matrix[A] if row != v.row || col != v.col  => false
     case s: Matrix[A] => this.Values == s.Values
     case _ => false
   }
 
+  /***
+   * Method to return the matrix in string format
+   * @return String format of the matrix
+   */
   override def toString: String = values.map(x =>  "[" + x.mkString(", ") + "]").mkString("\n")
 }
 
